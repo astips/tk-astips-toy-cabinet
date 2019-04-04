7f193e755fdc00e2f52416877c90412945ecf8ea
astips <thebeatles2006@163.com>
2019-04-04 23:17:55 +0800

    add user 'tag group permission' control.

--- toycabinet/__init__.py
+++ toycabinet/__init__.py
@@ -4,7 +4,7 @@ __name__ = 'ToyCabinet'
 __version__ = '1.0.0'
 __author__ = 'astips'
 __package__ = 'toycabinet'
-__build__ = 'Cython 0.29, build on March 27, 2019'
+__build__ = 'Cython 0.29, build on April 04, 2019'
 
 
 from .core.dborm.const import db_sql
